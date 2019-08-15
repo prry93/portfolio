b4e73231d5daf4431abf995b0836ceef5b888e08
perry93 <prry93@gmail.com>
2019-08-14 22:47:31 -0700

    moved images to new file. Added section for screenshots

--- client/src/components/portfolio/portfolio.js
+++ client/src/components/portfolio/portfolio.js
@@ -2,24 +2,20 @@ import React, { Component } from 'react'
 import "./portfolio.css"
 import Carousel from 'nuka-carousel'
 
-import giphy from "../../images/giphycard.png"
-import gem from "../../images/gemcard.png"
-import cssGrid from "../../images/cssgrid.png"
-import igloo from "../../images/igloocard.png"
-import arrowF from "../../images/arrowF.png"
-import arrowB from "../../images/arrowB.png"
+import {img0, img1, img2, img3, img4, giphy, gem, cssGrid, igloo, arrowF, arrowB} from "./imageimport"
 
 
 class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      slideIndex: 1,
+      slideIndex: 0,
       length: 5,
       slidesToShow: 3,
       heightMode: "max",
       transitionMode: "scroll3d",
       wrapAround: true,
+      opacityScale: 0.5
     }
     this.nextSlide = this.nextSlide.bind(this);
     this.previousSlide = this.previousSlide.bind(this);
@@ -49,21 +45,27 @@ class Portfolio extends Component {
 // If statements will cycle the carousel back to the end/beginning.
 
   render() {
+    const images = [img0, img1, img2, img3, img4];
+    // Assigned images to a variable since React won't directly let me render an array of objects
+    const screencaps = images[this.state.slideIndex];
+    // screencaps will pair the src of each image with with the slide index. In the return, a img tag will display the correct image associated with the index.
+
     return (
       <div>
 
         <div className="wrapper">
           <div className="screen">
-            <div className="screenContainer"></div>
+            <img className="screenContainer" src={screencaps} alt="screenshots of projects" />
           </div>
           <div className="content">
-            <p>djfdkjfd</p>
+            <img src={screencaps} alt="text content"/>
           </div>
           <div className="carousel" > 
             <Carousel 
               slidesToShow={this.state.slidesToShow}
               transitionMode={this.state.transitionMode}
               slideIndex={this.state.slideIndex}
+              opacityScale={this.state.opacityScale}
               wrapAround={this.state.wrapAround}
               renderCenterLeftControls={ () => (
                   <button className="leftButton"  onClick={this.previousSlide}><img src={arrowB} alt="backwards arrow"></img></button>
