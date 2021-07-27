5c283e08264a3d5490bdb7c80e727bc50d9e0a3a
perry93 <prry93@gmail.com>
2021-07-27 14:30:43 -0700

    fixed keys

--- server/config.js
+++ server/config.js
@@ -1,8 +1,8 @@
-const user = 'prry93@gmail.com';
-const refreshToken = '1/C-cZy-YSrcSOAFVEfGl4PVsG78-cCls2fuDxGGBrYu4';
-const accessToken = 'ya29.GltaB3H8ubFh09AFdfcPSRElja1BEdvNyM4NtolzzDT93b1gdCEeaAAdc1_9yOmbtx8ObdW65k3T8b6JpSA6j5CFcnDG-9Pau5CX55VaDan_MRkaG1VL3Inpt4u5';
-const clientId = "716756980960-8lger2rij0bm7h53qchmt7uhv4e96hn2.apps.googleusercontent.com";
-const clientSecret = "0Ure6hRJpYF9PlVWqs942uMM";
+const user = '';
+const refreshToken = '';
+const accessToken = '';
+const clientId = "";
+const clientSecret = "";
 
 module.exports = {
   user,
